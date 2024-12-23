e0e60c4a21a9f3967efcd738f5efc6bdca0491c8
pawel-wieczynski <94794405+pawel-wieczynski@users.noreply.github.com>
2024-12-23 07:55:49 +0100

    classes for reading, preprocessing and embedding texts

---
+++ word_embeddings.py
@@ -0,0 +1,73 @@
+import requests
+import re
+from gensim.models.keyedvectors import load_word2vec_format
+import numpy as np
+# import nltk
+from nltk.corpus import stopwords
+# nltk.download("stopwords")
+
+class TextReader:
+    """
+    Downloading data from Project Gutenberg.
+    """
+
+    def __init__(self, book_id: str) -> None:
+        self.book_id = book_id
+        self.url = f"https://www.gutenberg.org/cache/epub/{book_id}/pg{book_id}.txt"
+        self.text = None
+    
+    def download_text(self) -> str:
+        response = requests.get(self.url)
+        self.text = response.text
+
+class TextPreprocessor:
+    """
+    Preprocessing steps:
+        - Convert to lowercase (embeddings and stop words should be lowercase as well).
+        - Remove special character and numbers / keep only Latin letters.
+        - Tokenize text.
+        - Remove stop words.
+    """
+
+    def __init__(self, language: str) -> None:
+        self.stop_words = stopwords.words(language)
+    
+    def preprocess_text(self, text: str) -> list[str]:
+        text = text.lower()
+        text = re.sub(r"[^a-z\s]", "", text)
+        tokens = text.split()
+        tokens = [word for word in tokens if word not in self.stop_words and len(word) > 1]
+        return tokens
+
+class WordEmbeddings:
+    def __init__(self, embedding_path: str) -> None:
+        self.embedding_path = embedding_path
+        self.embeddings = None
+    
+    def load_embeddings(self) -> None:
+        self.embeddings = load_word2vec_format(self.embedding_path, binary = True)
+
+    def embed_text(self, tokens: list[str]) -> list[np.ndarray]:
+        return [self.embeddings[word] for word in tokens if word in self.embeddings]
+
+    def calculate_coverage(self, tokens: list[str]) -> float:
+        n_tokens = len(tokens)
+        n_covered = sum(token in self.embeddings for token in tokens)
+        return n_covered / n_tokens if n_tokens > 0 else 0.0
+
+test = TextReader("1513")
+test.download_text()
+# print(test.text)
+# sw = ['the', 'or', 'of', 'and']
+test_preprocess = TextPreprocessor("english")
+tokens = test_preprocess.preprocess_text(test.text)
+# print(tokens)
+word_embeddings = WordEmbeddings("model.bin")
+word_embeddings.load_embeddings()
+vectors = word_embeddings.embed_text(tokens)
+print(len(tokens))
+print(len(vectors))
+print(vectors[0].shape)
+print(word_embeddings.calculate_coverage(tokens))
+# print(vectors[0:10])
+# print(test_preprocess.tokens)
