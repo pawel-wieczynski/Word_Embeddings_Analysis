a6fd2e4baa99d82230dbba836b0ecf5f1ba7235b
pawel-wieczynski <94794405+pawel-wieczynski@users.noreply.github.com>
2025-01-24 07:26:24 +0100

    Lindstone smoothing

--- scripts/mutual_information.py
+++ scripts/mutual_information.py
@@ -3,10 +3,12 @@ from numpy import log2, log
 from scipy.special import digamma
 
 class MutualInformation:
-    def __init__(self, tokens: list[str]) -> None:
+    def __init__(self, tokens: list[str], alpha: float = None) -> None:
         self.tokens: list[str] = tokens
         self.tokens_length = len(self.tokens)
         self.tokens_counts = self.count_tokens()
+        self.vocabulary_size = len(self.tokens_counts)
+        self.alpha = alpha
     
     def count_tokens(self):
         counts = defaultdict(int)
@@ -29,11 +31,25 @@ class MutualInformation:
         return joint_counts
     
     def get_marginal_probability(self, word: str) -> float:
-        return self.tokens_counts[word] / self.tokens_length
+        """
+        Lindstone smoothed marginal probability:
+            (count(word) + alpha) / (N + alpha * m)
+        """
+        if self.alpha is None:
+            return self.tokens_counts[word] / self.tokens_length
+        else:
+            return (self.tokens_counts[word] + self.alpha) / (self.tokens_length + self.alpha * self.vocabulary_size)
     
     def get_joint_probability(self, word_x: str, word_y: str, joint_counts) -> float:
         number_of_pairs = sum(joint_counts.values())
-        return joint_counts[(word_x, word_y)] / number_of_pairs if number_of_pairs > 0 else 0.0
+        if number_of_pairs == 0:
+            return 0.0
+        
+        if self.alpha is None:
+            return joint_counts[(word_x, word_y)] / number_of_pairs
+        else:
+            m = len(joint_counts)
+            return (joint_counts[(word_x, word_y)] + self.alpha) / (number_of_pairs + self.alpha * m)
     
     def calculate_mutual_information(self, lag: int):
         joint_counts = self.count_joint_tokens(lag = lag)
@@ -44,7 +60,7 @@ class MutualInformation:
         
         mi = 0.0
         for (x, y), count_xy in joint_counts.items():
-            p_xy = count_xy / number_of_pairs
+            p_xy = self.get_joint_probability(word_x = x, word_y = y, joint_counts = joint_counts)
             p_x = self.get_marginal_probability(word = x)
             p_y = self.get_marginal_probability(word = y)
             if (p_xy > 0) and (p_x > 0) and (p_y > 0):
